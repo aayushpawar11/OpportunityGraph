66ed1104f02809c29825e0561e2db1879bd75d74
aayushpawar11 <117047168+aayushpawar11@users.noreply.github.com>
2025-05-30 23:33:39 -0400

    frontend react, vite, tailwind

---
+++ frontend/src/App.jsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="text-center p-10 text-3xl text-blue-600 font-bold">
+      🎓 OpportunityGraph is Live with Tailwind!
+    </div>
+  )
+}
+
+export default App
